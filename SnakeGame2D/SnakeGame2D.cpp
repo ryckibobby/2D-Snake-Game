42e3a21827e1d3dd79c605321d378ab242949365
Rycki Harding <ryckiharding@gmail.com>
2024-08-31 14:34:19 -0400

    update C++ define game area variables directions

--- SnakeGame2D/SnakeGame2D.cpp
+++ SnakeGame2D/SnakeGame2D.cpp
@@ -1,9 +1,42 @@
 // SnakeGame2D.cpp 
 
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+
+
+//define game area size
+const int width = 20;
+const int height = 17;
+
+//variable for snake and food position
+int x, y, fruitX, fruitY, score;
+
+//track snake's tail
+int tailX[100], tailY[100];
+int nTail;
+
+
+//direction control
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+//game over 
+bool gameOver;
+
+void Setup() {
+	gameOver = false; 
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
 
