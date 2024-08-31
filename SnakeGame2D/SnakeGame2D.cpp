2fa97a455f828673dc8e46ddcd2919cd85474fe8
Rycki Harding <ryckiharding@gmail.com>
2024-08-31 17:19:00 -0400

    update C++ logic updates snakes position & handles collision main function runs game loop

--- SnakeGame2D/SnakeGame2D.cpp
+++ SnakeGame2D/SnakeGame2D.cpp
@@ -99,8 +99,71 @@ void Input() {
 		}
 	}
 }
+
+void Logic() {
+	//track previous positions
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
+	//update snake's head position based on the direction
+	switch (dir) {
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
+		default:
+			break;
+	}
+	
+	//check if snake hits the wall
+	if (x >= width) x = 0; else if (x < 0) x = width - 1;
+	if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+	//check if snake hits iself
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y) {
+			gameOver = true;
+		}
+	}
+
+	//check if snake eats the fruit
+	if (x == fruitX && y == fruitY) {
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		nTail++;
+	}
+}
+
+
 int main()
 {
 	Setup();
+	while (!gameOver) {
+		Draw();
+		Input();
+		Logic();
+		Sleep(100);
+	}
+	return 0;
 }
 
